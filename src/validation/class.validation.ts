67e6f4693e08ca3defb3b82482f40f3a019b9480
TharinduH <hettipower@gmail.com>
2025-04-09 17:44:44 +0530

    classes validation updated

--- src/validation/class.validation.ts
+++ src/validation/class.validation.ts
@@ -1,12 +1,12 @@
 import * as Joi from 'joi';
 
 export const CreateClassSchema = Joi.object({
-  class_name: Joi.string().min(3).max(30).required(),
-  teacher: Joi.number().min(3).max(30).required(),
-  subject: Joi.number().min(3).max(30).required(),
-  registrationAmount: Joi.number().min(3).max(30).required(),
-  classFeeAmount: Joi.number().min(3).max(30).required(),
-  commission: Joi.number().min(3).max(30).required(),
+  class_name: Joi.string().min(3).required(),
+  teacher: Joi.number().required(),
+  subject: Joi.number().required(),
+  registrationAmount: Joi.number().min(3).required(),
+  classFeeAmount: Joi.number().min(3).required(),
+  commission: Joi.number().min(1).required(),
   registrations: [Joi.number()],
   classFees: [Joi.number()],
 });
