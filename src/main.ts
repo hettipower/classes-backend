5b8ffa59669c56c6dab641525ebfe999c5e24cd8
TharinduH <hettipower@gmail.com>
2025-04-09 11:32:52 +0530

    swagger added and config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS with default settings
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Classes API')
+    .setDescription('The classes API description')
+    .setVersion('1.0')
+    .addTag('classes')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
